ea2e723a3b38a46815f0caef30177d3b0a30f7c1
vtearit <vuongvuduc.se@gmail.com>
2021-07-07 11:34:26 +0700

    Edit view mode category page

--- app/etc/config.php
+++ app/etc/config.php
@@ -317,6 +317,7 @@ return [
         'Magento_WishlistAnalytics' => 1,
         'Magento_WishlistGraphQl' => 1,
         'Magento_WishlistSampleData' => 1,
+        'AHT_AttributeCustomer' => 1,
         'Amazon_Core' => 1,
         'Amazon_Login' => 1,
         'Amazon_Payment' => 1,
